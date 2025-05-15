4bf5e5503514250ef521a3251ce2fac0020c9ed7
Emil <somaleyoo123@gmail.com>
2025-05-15 10:29:56 +0200

    main css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Main from './Components/Acceuil/acceuil'
 
 function App() {
 
 
   return (
     <>
-      
+      <Main score="10"/>
     </>
   )
 }
--- src/Components/Acceuil/acceuil.jsx
+++ src/Components/Acceuil/acceuil.jsx
@@ -3,7 +3,20 @@ import './acceuil.css'
 export default function Main(props) {
     return(
         <>
-            
+            <div id="score">
+                <div className="titre">
+                    <h1>ROCK</h1>
+                    <h1>PAPER</h1>
+                    <h1>SCISSORS</h1>
+                </div>
+                <div className="resultat">
+                    <h2>Score : </h2>
+                    <h1 className='score'>{props.score}</h1>
+                </div>
+            </div>
+            <div className="jeu">
+
+            </div>
         </>
     )
 }
